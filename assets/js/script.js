f3c56f9990af150a9b97afbc8023e005a4287c47
sarahm <minutemin@gmail.com>
2023-05-17 22:02:07 -0700

    edit hours to 9-5p

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
 
-/* jQuery alternative all in function:  $(document).ready(function () {
-   loadStorageData();  */
-
 //set current date and time on the banner
 $('#currentDay').text(dayjs().format('[Today\'s Date and Time: ]dddd, MMMM D, YYYY - hh:mma'));
-
 
 $(document).ready(function () {
     // set colors for time in past, present and future. 
     function setHourColors() {
         var now = dayjs();
 
-        for (var i = 15; i < 24; i++) {
+        for (var i = 9; i < 18; i++) {
             //$("#hour-" + i + "textarea").removeClass("future");
+            console.log(i);
             if (i < now.hour()) {
                 $("#hour-" + i + " textarea").addClass("past");
             } else if (i == now.hour()) {
@@ -27,32 +24,31 @@ $(document).ready(function () {
     function loadStorageData() {
         eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
         console.log(eventsData);
-        //         if (!eventsData) {
-        //     eventsData = {
-        //         hour15: "",
-        //         hour16: "",
-        //         hour17: "",
-        //         hour18: "",
-        //         hour19: "",
-        //         hour20: "",
-        //         hour21: "",
-        //         hour22: "",
-        //         hour23: "",
-        //         hour24: "",
-        //     }
-        // }
+                if (!eventsData) {
+            eventsData = {
+                hour9: "",
+                hour10: "",
+                hour11: "",
+                hour12: "",
+                hour13: "",
+                hour14: "",
+                hour15: "",
+                hour16: "",
+                hour17: "",
+            }
+        }
         // //get any info from local storage
-        console.log(localStorage.getItem("hour15")); 
-        console.log($('#hour-15 .item')); 
+        // console.log(localStorage.getItem("hour-9")); 
+        // console.log($('#hour-9 .item')); 
+        $("#hour-9 .item").val(eventsData.hour9);
+        $("#hour-10 .item").val(eventsData.hour10);
+        $("#hour-11 .item").val(eventsData.hour11);
+        $("#hour-12 .item").val(eventsData.hour12);
+        $("#hour-13 .item").val(eventsData.hour13);
+        $("#hour-14 .item").val(eventsData.hour14);
         $("#hour-15 .item").val(eventsData.hour15);
         $("#hour-16 .item").val(eventsData.hour16);
         $("#hour-17 .item").val(eventsData.hour17);
-        $("#hour-18 .item").val(eventsData.hour18);
-        $("#hour-19 .item").val(eventsData.hour19);
-        $("#hour-20 .item").val(eventsData.hour20);
-        $("#hour-21 .item").val(eventsData.hour21);
-        $("#hour-22 .item").val(eventsData.hour22);
-        $("#hour-23 .item").val(eventsData.hour23);
     }
 
     // function for saving the string into local storage as a JSON strink calendar events
@@ -79,15 +75,18 @@ $(document).ready(function () {
         //store in local Storage
         localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
     }
+
     //call function on click save button.
-    
     $(".saveBtn").on("click", handleSaveClick);
     $(".container").on("click", handleSaveClick); 
+
     //calls the load storage data and hours functions
-    $(function () {
-        loadStorageData();
-        setHourColors();
-    });
+    // $(function () {
+    //     loadStorageData();
+    //     setHourColors();
+    // });
+    loadStorageData();
+    setHourColors();
 });
 
 
